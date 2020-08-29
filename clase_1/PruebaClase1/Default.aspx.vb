66803857e4578746a62c5eff129272642a0bc100
BAUBYTE <paredbaez.martin@gmail.com>
2020-08-29 12:26:57 -0300

    Clase 1

---
+++ clase_1/PruebaClase1/Default.aspx.vb
@@ -0,0 +1,14 @@
+﻿Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClic.Click
+    End Sub
+
+    Protected Sub txtPrueba_TextChanged(sender As Object, e As EventArgs) Handles txtPrueba.TextChanged
+        lblPrueba.Text = txtPrueba.Text
+    End Sub
+End Class
