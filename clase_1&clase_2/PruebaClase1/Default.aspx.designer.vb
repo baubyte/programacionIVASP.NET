5237b659664bb3954488da272f19226a5516084e
BAUBYTE <paredbaez.martin@gmail.com>
2020-09-05 11:52:01 -0300

    Clase 2

--- clase_1&clase_2/PruebaClase1/Default.aspx.designer.vb
+++ clase_1&clase_2/PruebaClase1/Default.aspx.designer.vb
@@ -23,6 +23,15 @@ Partial Public Class _Default
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
 
     '''<summary>
+    '''Control lblHora.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblHora As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
     '''Control lblPrueba.
     '''</summary>
     '''<remarks>
--- clase_1&clase_2/PruebaClase1/Default.aspx.vb
+++ clase_1&clase_2/PruebaClase1/Default.aspx.vb
@@ -2,10 +2,13 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If Not IsPostBack Then
+            lblHora.Text = DateTime.Now.ToString("HH:mm:ss") & " Esta hora es del Page Load"
+        End If
     End Sub
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClic.Click
+        'lblHora.Text = DateTime.Now.ToString("HH:mm:ss")
     End Sub
 
     Protected Sub txtPrueba_TextChanged(sender As Object, e As EventArgs) Handles txtPrueba.TextChanged
